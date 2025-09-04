dcd8a41410c59cae42e5801df8fbdcbb85c63798
Vamsi Dogiparthi <30088887@brighthorizons.com>
2025-09-04 15:49:57 -0400

    Finished backend. Working on the UI.

---
+++ MainMicroserviceECommerceProject/src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
+public record GetBasketResponse(ShoppingCartModel Cart);
+
+public record StoreBasketRequest(ShoppingCartModel Cart);
+public record StoreBasketResponse(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
---
+++ MainMicroserviceECommerceProject/src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Models.Catalog;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Categories { get; set; } = [];
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; }
+    public string ImageFile { get; set; } = default!;
+}
+
+//wrapper classes for responses
+public record GetProductsResponse(IEnumerable<ProductModel> Products);
+public record GetProductByIdResponse(ProductModel Product);
+public record GetProductsByCategoryResponse(IEnumerable<ProductModel> Products);
